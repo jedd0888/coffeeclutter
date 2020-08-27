6424cae71c0ef8cc5c18012973e760473bef4bd4
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 18:06:26 +1000

    v1.1

---
+++ commands/bobux.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    message.reply("https://cdn.discordapp.com/attachments/746656556187123772/746658769135730738/video0_2.mp4")
+}
+
+module.exports.config = {
+    name: "bobux",
+    description: "free bobux",
+    usage: "c!bobux",
+    accessableby: "Members",
+    aliases: ["robux", "bobuxdance"]
+}
