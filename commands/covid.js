4dc43e7e42b3ca502d8d137f4324babe0f883ec9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 19:06:32 +1000

    v1.3.1

--- commands/covid.js
+++ commands/covid.js
@@ -1,60 +1,30 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch')
+const covid = require('novelcovid');
 
 module.exports.run = async (bot, message, args) => {
-    let countries = args.join(" ");
+    const covidStats = await covid.all()
 
-    //Credit to Sarastro#7725 for the command :)
-
-    const noArgs = new Discord.MessageEmbed()
-        .setTitle('Missing arguments')
-        .setColor(0xFF0000)
-        .setDescription('You are missing some args (ex: ;covid all || ;covid Canada)')
-        .setTimestamp()
-
-    if (!args[0]) return message.channel.send(noArgs);
-
-    if (args[0] === "all") {
-        fetch(`https://covid19.mathdro.id/api`)
-            .then(response => response.json())
-            .then(data => {
-                let confirmed = data.confirmed.value.toLocaleString()
-                let recovered = data.recovered.value.toLocaleString()
-                let deaths = data.deaths.value.toLocaleString()
-
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`Worldwide COVID-19 Stats 🌎`)
-                    .addField('Confirmed Cases', confirmed)
-                    .addField('Recovered', recovered)
-                    .addField('Deaths', deaths)
-
-                message.channel.send(embed)
-            })
-    } else {
-        fetch(`https://covid19.mathdro.id/api/countries/${countries}`)
-            .then(response => response.json())
-            .then(data => {
-                let confirmed = data.confirmed.value.toLocaleString()
-                let recovered = data.recovered.value.toLocaleString()
-                let deaths = data.deaths.value.toLocaleString()
-
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`COVID-19 Stats for **${countries}**`)
-                    .addField('Confirmed Cases', confirmed)
-                    .addField('Recovered', recovered)
-                    .addField('Deaths', deaths)
-
-                message.channel.send(embed)
-            }).catch(e => {
-                return message.channel.send('Invalid country provided')
-            })
-    }
+    return message.channel.send(new Discord.MessageEmbed()
+        .setTitle('COVID-19 Stats')
+        .setColour('#C73B0F')
+        .addFields(
+            { name: `Cases`, value: covidStats.cases.toLocaleString(), inline: true },
+            { name: `Cases Today`, value: covidStats.todayCases.toLocaleString(), inline: true },
+            { name: `Deaths`, value: covidStats.death.toLocaleString(), inline: true },
+            { name: `Deaths Today`, value: covidStats.todayDeaths.toLocaleString(), inline: true },
+            { name: `Recovered`, value: covidStats.recovered.toLocaleString(), inline: true },
+            { name: `Recovered Today`, value: covidStats.todayRecovered.toLocaleString(), inline: true },
+            { name: `Infected Right Now`, value: covidStats.active.toLocaleString(), inline: true },
+            { name: `Critical Condition`, value: covidStats.critical.toLocaleString(), inline: true },
+            { name: `Tested`, value: covidStats.tests.toLocaleString(), inline: true }
+        )
+    )
 }
 
 module.exports.config = {
     name: "covid",
-    description: "COVID-19 stats on a country or worldwide.",
-    usage: "c!covid all || c!covid {country}",
+    description: "COVID-19 stats.",
+    usage: "c!covid",
     accessableby: "Members",
-    aliases: ["covid19", "coronavirus", "corona"]
+    aliases: ["corona", "covid19", "covid-19", "covidstats"]
 }
