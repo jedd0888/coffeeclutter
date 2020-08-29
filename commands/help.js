dd719d8897392f7ebfb41346e2f72b8bab2cb5e1
BuildTools <unconfigured@null.spigotmc.org>
2020-08-29 21:46:37 +1000

    v1.8

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     if (!helpArgs[0]) {
         var embed = new Discord.MessageEmbed()
             .setAuthor(`List of avaliable commands:`)
-            .setDescription('```avatar | bobux | covid | dankmeme | facebookmeme | giveaway | meme | nickname | poll | purge | weather```')
+            .setDescription('```avatar | bobux | covid | dankmeme | facebookmeme | giveaway | meme | purge```')
             .addFields({ name: 'Prefix', value: '```c!```', inline: true })
             .setColor('#C73B0F')
 
